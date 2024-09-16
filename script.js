3edc06a20e5549cfbdfd2d75bedc7c0678987596
Neeraj7275 <neeraj17022004@gmail.com>
2024-09-16 23:58:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let disp = document.querySelector(".inptdisplay");
+
+function sin(){
+    disp.value = Math.sin(disp.value);
+}
+function cos(){
+    disp.value = Math.cos(disp.value);
+}
+
+function tan(){
+    disp.value = Math.tan(disp.value);
+}
+
+function sqrt(){
+    disp.value = Math.sqrt(disp.value);
+}
